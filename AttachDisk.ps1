af34036db956be3cd813e910c985dda52fbaae94
Aakash Masand <akash@techconnect.com.au>
2018-05-29 15:27:14 +1000

    Added script AttachDisk.ps1

---
+++ AttachDisk.ps1
@@ -0,0 +1,14 @@
+﻿# -------------------------------------------------------------------------------------------------
+# Script: AttachDisk.ps1
+# Author: Akash Masand
+# Date: 29/05/2018 15:24:00 hrs
+# Keywords: Custom Script Extensions, Attach Disks, Data Disks
+# Comments:
+# do not delete. This script is being used by an ARM template
+# -------------------------------------------------------------------------------------------------
+$offlinedisks = get-disk | where OperationalStatus -EQ offline
+foreach ($disk in $offlinedisks)
+{
+    Set-Disk -Number $disk.Number -IsOffline $false 
+    Set-Disk -Number $disk.Number -IsReadOnly $false
+}
